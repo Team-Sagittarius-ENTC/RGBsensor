cf96c2746fd2fa0f29c6f75e31b39c79ef9473fd
0301yasiru <0301yasiru@gmail.com>
2021-05-19 09:02:55 +0530

    AVR code

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,17 +1,18 @@
-#include <LiquidCrystal.h>
 #include "RGBsensor.h"
 #include "keypad.h"
+#include "RGBled.h"
 
 byte ldrPins[3] = {A2, A1, A0};
 int *color {nullptr};
 
+// creating instances of classes we defined
 RGBsensor Sensor(ldrPins);
 Keypad keypad(A3);
-LiquidCrystal lcd(2,3,4,5,6,7);
+RGBled Led;
+
 
 void setup(){
   Serial.begin(9600);
-  lcd.begin(16, 2);
 }// end of the void setup function
 
 void loop(){
